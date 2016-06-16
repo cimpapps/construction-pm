f681787e66148caa7f09897ac2000a230660db90
catalincimpoeru24@gmail.com <catalincimpoeru24@gmail.com>
2016-06-16 11:23:03 +0300

    Singleton EntityMangerFactory

---
+++ src/main/java/com/cimpapps/construction/pm/service/EntityManagerFactorySingleton.java
@@ -0,0 +1,25 @@
+
+package com.cimpapps.construction.pm.service;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerFactorySingleton {
+
+    private static final String PU_NAME = "com.cimpapps_construction-pm-db_jar_1.0-SNAPSHOTPU";
+    
+    private EntityManagerFactory emf;
+    
+    private EntityManagerFactorySingleton(){
+        emf = Persistence.createEntityManagerFactory(PU_NAME);
+    }
+    
+    private static final class SingletonHolder{
+        private static final EntityManagerFactorySingleton INSTANCE = new EntityManagerFactorySingleton();
+    }
+    
+    public static EntityManagerFactorySingleton getInstance(){
+        return SingletonHolder.INSTANCE;
+    }
+    
+}
